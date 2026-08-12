c2ea73b391c19703c08cc3acd7c9ce3209a655d9
kakka2307-design <kakka2307@gmail.com>
2026-08-12 22:30:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById("whatsappBtn");
+
+btn.href = "https://wa.me/?text=" + encodeURIComponent(
+"You're invited to our wedding 💍\n\n" +
+"Varsha & Vairav\n\n" +
+"📅 16 November 2026\n" +
+"📍 Puduvayal\n\n" +
+"View the invitation:\n" +
+"https://kakka2307-design.github.io/wedding-site/"
+);
